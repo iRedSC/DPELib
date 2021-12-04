910819d9ce0b239431782712a91f127268385b7b
iRedSC <redstonecharger@gmail.com>
2021-12-03 23:46:09 -0500

    Update bash

--- lectern/build-all.bash
+++ lectern/build-all.bash
@@ -1,3 +1,3 @@
-cd "/Users/redsc/Documents/Datapacks/DPELib/lectern"
+cd "$(dirname ${BASH_SOURCE[0]})"
 lectern template.md -d built/template.zip
 lectern making_a_datapack.md -d built/making_a_datapack.zip
